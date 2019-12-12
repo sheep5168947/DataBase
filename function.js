3bae5cc2d575db718df6979260ae9526255b3942
KenPan <sheep5168947@gmail.com>
2019-12-12 21:11:03 +0800

    asd

---
+++ function.js
@@ -0,0 +1,30 @@
+
+////////////////////////////////////////////////
+//login.html
+function login() {
+    console.log("fuck");
+
+}
+
+function SigninSuccess() {
+    window.location.href = "../main page/main.html";
+}
+function Nickname() {
+    if (document.getElementById('Nickname').value == "") {
+
+    }
+    else {
+        window.location.href = "../main page/main.html";
+        var name = document.getElementById("Nickname").value;
+        document.getElementById("hahaha").innerHTML = document.getElementById("Nickname").value;
+    }
+
+}
+////////////////////////////////////////////////
+//main.html
+
+
+////////////////////////////////////////////////
+//Producer
+
+////////////////////////////////////////////////
