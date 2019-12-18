5b87f2eb09bed499add1bba818e3009b2653bc9f
lulalabana <k11543789@gmail.com>
2019-12-18 23:50:06 +0800

    重新啟動

---
+++ DB_server/mysite/MinecraftDB/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Terrain
+from .models import Attack_Creature
+# Register your models here.
+admin.site.register(Terrain)
+admin.site.register(Attack_Creature)
---
+++ DB_server/mysite/MinecraftDB/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from .models import Terrain
+from django.db import connection,transaction
+
+def index(request):
+    return render(request,'MinecraftDB/index.html')
+
+def Search(request, search_name):
+    cursor=connection.cursor()
+    cursor.execute('select Introduction_text from MinecraftDB_Terrain')
+    search_object=cursor.fetchone()[0]
+
+    # search_object = Terrain.objects.get(Terrain, Terrain_name=search_name)
+    return render(request, 'MinecraftDB/index.html', {'search_object': search_object})
+
+def reply_login(request):
+    if(request.method=="POST"):
+        # 輸入帳號密碼
+        if('account' in request.POST):
+            #print(request.POST['account'])
+            # 確認有沒有輸入正確
+            if(request.POST['account']=="" or request.POST['password']==""):
+                return render(request, 'MinecraftDB/login.html',{'err_acc':"請輸入正確的 account 或 password"})
+            #確認有沒有此人
+            cursor=connection.cursor()
+            cursor.execute("select Member_name from MinecraftDB_member where Account_number LIKE '"+request.POST['account']+"' and Password LIKE '"+request.POST['password']+"'")
+            # cursor.execute("select Member_name from MinecraftDB_member where Account_number LIKE '%s' and Password LIKE '%s'",["ppaa","ppaa"])
+            ans=cursor.fetchone()
+            #print(ans)
+            if ans is None:
+                print("no ans")
+                return render(request, 'MinecraftDB/login.html',{'err_acc':"請輸入正確的 account 或 password"})
+            else :
+                return redirect("/MinecraftDB/main/"+ans[0]+"/")         
+        if('nickname' in request.POST):
+            #print(request.POST['nickname'])
+            if(request.POST['nickname']==""):
+                return render(request, 'MinecraftDB/login.html',{'no_nike':"請輸入nickname"})
+        # DB尋找
+
+
+def reply_post(request):
+    cursor=connection.cursor()
+    title=request.POST['title']
+    context=request.POST['Contents']
+    print(title+" "+context)
+    cursor.execute("INSERT INTO MinecraftDB_member_diary (Title, Diary,Member_Diary_name) values (%s, %s,%s)", [title,"someone", context])
+    #return render(request, 'MinecraftDB/login.html')
+    return redirect("/MinecraftDB/main/")
+
+def login(request):
+    return render(request, 'MinecraftDB/login.html')
+
+def main(request,username):
+    cursor=connection.cursor()
+    cursor.execute('select Introduction_text from MinecraftDB_Terrain')
+    search_object=cursor.fetchone()[0]
+    return render(request, 'MinecraftDB/main.html',{'Username':username})
+
+def producer(request):
+    return render(request, 'MinecraftDB/producer.html')
+
+def post(request):
+    return render(request, 'MinecraftDB/post.html')
+
+def ALLpost(request):
+    List=[{'Poster':"willy",'Title':"loooooool",'content':"nooo"}]
+    return render(request, 'MinecraftDB/Allpost.html',{'List':List,'Username':'lulalabana'})
