19a8cc2e445ebbbbce591b4950b38b1bcc6c23af
xxxddd12xd <xxxddd12xd@gmail.com>
2019-12-18 23:42:50 +0800

    重新G8

--- DB_server/mysite/MinecraftDB/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Terrain
-from .models import Attack_Creature
-# Register your models here.
-admin.site.register(Terrain)
-admin.site.register(Attack_Creature)
