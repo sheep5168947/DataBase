85b1ba57bd3bf8252acde104d9c71139cfc458be
KenPan <sheep5168947@gmail.com>
2019-12-19 21:25:53 +0800

    ddd

--- DB_server/mysite/MinecraftDB/templates/static/post/js/post.js
+++ DB_server/mysite/MinecraftDB/templates/static/post/js/post.js
@@ -6,3 +6,7 @@ function deletpost(){
     document.getElementById("addpost").style.display = "none";
     document.getElementById("deletpost").style.display = "block";
 }
+
+var test1=document.querySelector("mr-2 d-none d-lg-inline text-gray-600 small")
+console.log(test1)
+console.log("1")
