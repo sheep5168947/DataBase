d5874d2180ed257b753af66010b45e948a716cf0
lulalabana <k11543789@gmail.com>
2019-12-19 19:09:54 +0800

    html_upup

---
+++ DB_server/mysite/MinecraftDB/templates/static/Allpost/js/post.js
@@ -0,0 +1,8 @@
+function addpost(){
+    document.getElementById("addpost").style.display = "block";
+    document.getElementById("deletpost").style.display = "none";
+}
+function deletpost(){
+    document.getElementById("addpost").style.display = "none";
+    document.getElementById("deletpost").style.display = "block";
+}
