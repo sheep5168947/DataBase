995cde9ed2e44e0f55c6a31f68a9f6929e3505f8
KenPan <sheep5168947@gmail.com>
2020-01-03 01:24:46 +0800

    dsds

--- DB_server/mysite/MinecraftDB/templates/static/main_page/js/Steve.js
+++ DB_server/mysite/MinecraftDB/templates/static/main_page/js/Steve.js
@@ -1,33 +1,225 @@
-function AttackAdder(add) {
-    var hoe = add.toElement.id
-    var spliter = hoe.split("-")
-    var attack = spliter[1]
-    var atkTable = ["0", "4", "4", "5", "6", "7", "9", "7", "7", "7", "9", "9", "9"]
-    console.log(atkTable[attack])
+function AttackAdder() {
+    
+    var atkTable = ["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0", "4", "4", "5", "6", "7", "9", "7", "7", "7", "9", "9", "9"]
+   
+    var mabiyoung = 0;
+
+    for(var i=22;i<34;i++)
+    {
+        if(showheadimg[i].style.visibility=="visible")
+        {
+            mabiyoung = atkTable[i];
+            break;
+        }
+    }
+    ohnice.innerHTML=("Steve攻擊力")+mabiyoung
 }
 
-var weapon1 = document.getElementById("weapon-1")
-var weapon2 = document.getElementById("weapon-2")
-var weapon3 = document.getElementById("weapon-3")
-var weapon4 = document.getElementById("weapon-4")
-var weapon5 = document.getElementById("weapon-5")
-var weapon6 = document.getElementById("weapon-6")
-var weapon7 = document.getElementById("weapon-7")
-var weapon8 = document.getElementById("weapon-8")
-var weapon9 = document.getElementById("weapon-9")
-var weapon10 = document.getElementById("weapon-10")
-var weapon11 = document.getElementById("weapon-11")
-var weapon12 = document.getElementById("weapon-12")
+function DefendAdder() {
+    console.log("fuck")
+    var mabiyoung = 0;
+    var atkTable = ["0","1","2","2","3","2","2","3","5","6","8","5","2","4","5","6","3","1","1","2","3","1"]
+    for(var i=1;i<22;i++)
+    {
+        if(showheadimg[i].style.visibility=="visible")
+        {
+            mabiyoung += parseInt(atkTable[i]) ;
+        }
+    }
+    
+    ohsiht.innerHTML=("Steve防禦力:")+mabiyoung
+}
+ var ohsiht=document.querySelector(".hoewantthemoney1")
+ var ohnice=document.querySelector(".hoewantthemoney2")
 
-weapon1.addEventListener("click", AttackAdder)
-weapon2.addEventListener("click", AttackAdder)
-weapon3.addEventListener("click", AttackAdder)
-weapon4.addEventListener("click", AttackAdder)
-weapon5.addEventListener("click", AttackAdder)
-weapon6.addEventListener("click", AttackAdder)
-weapon7.addEventListener("click", AttackAdder)
-weapon8.addEventListener("click", AttackAdder)
-weapon9.addEventListener("click", AttackAdder)
-weapon10.addEventListener("click", AttackAdder)
-weapon11.addEventListener("click", AttackAdder)
-weapon12.addEventListener("click", AttackAdder)
+
+
+var head = document.getElementById("head");
+var head_flag = 0;
+var showheadimg = document.getElementById("Steve").children;
+head.addEventListener("click", function () {
+    var showhead = parseInt(event.target.id);
+    if (head_flag == 0) {
+        event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+
+        showheadimg[showhead].style.visibility = "visible";
+        
+        head_flag = 1;
+        
+    } else if (head_flag == 1) {
+        var str1 = "";
+        str1 = event.target.style.boxShadow;
+        if (str1 == "rgba(51, 51, 51, 0.5) 0px 3px 15px 3px") {
+            event.target.style.boxShadow = "0 0 0 0";
+            showheadimg[showhead].style.visibility = "hidden";
+            head_flag = 0;
+            
+        } else {
+            var child = head.children;
+            for (var i = 0; i < child.length; i++) {
+                child[i].style.boxShadow = '';
+            }
+            for (var i = 1; i < 7; i++) {
+                showheadimg[i].style.visibility = "hidden";
+            }
+            showheadimg[showhead].style.visibility = "visible";
+            event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+            head_flag = 1;
+            
+        }
+    }
+    DefendAdder();
+})
+
+var breast = document.getElementById("breast");
+var breast_flag = 0;
+
+breast.addEventListener("click", function () {
+    var showhead = parseInt(event.target.id);
+    if (breast_flag == 0) {
+        event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+        showheadimg[showhead].style.visibility = "visible";
+        breast_flag = 1;
+        
+    } else if (breast_flag == 1) {
+        var str1 = "";
+        str1 = event.target.style.boxShadow;
+        if (str1 == "rgba(51, 51, 51, 0.5) 0px 3px 15px 3px") {
+            event.target.style.boxShadow = "0 0 0 0";
+            showheadimg[showhead].style.visibility = "hidden";
+            breast_flag = 0;
+            
+        } else {
+            var child = breast.children;
+            for (var i = 0; i < child.length; i++) {
+                child[i].style.boxShadow = '';
+            }
+            for (var i = 7; i < 12; i++) {
+                showheadimg[i].style.visibility = "hidden";
+            }
+            showheadimg[showhead].style.visibility = "visible";
+            event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+            breast_flag = 1;
+            
+        }
+
+    }
+    DefendAdder();
+
+})
+
+
+var leg = document.getElementById("leg");
+var leg_flag = 0;
+leg.addEventListener("click", function () {
+    var showhead = parseInt(event.target.id);
+    if (leg_flag == 0) {
+        event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+        showheadimg[showhead].style.visibility = "visible";
+        leg_flag = 1;
+        
+    } else if (leg_flag == 1) {
+        var str1 = "";
+        str1 = event.target.style.boxShadow;
+        if (str1 == "rgba(51, 51, 51, 0.5) 0px 3px 15px 3px") {
+            event.target.style.boxShadow = '0 0 0 0';
+            showheadimg[showhead].style.visibility = "hidden";
+
+            leg_flag = 0;
+            
+        } else {
+            var child = leg.children;
+            for (var i = 0; i < child.length; i++) {
+                child[i].style.boxShadow = '';
+            }
+            for (var i = 12; i < 17; i++) {
+                showheadimg[i].style.visibility = "hidden";
+            }
+            showheadimg[showhead].style.visibility = "visible";
+            event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+            leg_flag = 1;
+            
+        }
+
+
+    }
+    DefendAdder();
+})
+
+var feet = document.getElementById("feet");
+var feet_flag = 0;
+feet.addEventListener("click", function () {
+    var showhead = parseInt(event.target.id);
+    if (feet_flag == 0) {
+        event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+        showheadimg[showhead].style.visibility = "visible";
+        feet_flag = 1;
+        
+    } else if (feet_flag == 1) {
+        var str1 = "";
+        str1 = event.target.style.boxShadow;
+        if (str1 == "rgba(51, 51, 51, 0.5) 0px 3px 15px 3px") {
+            event.target.style.boxShadow = '0 0 0 0';
+            showheadimg[showhead].style.visibility = "hidden";
+            feet_flag = 0;
+            
+        } else {
+            var child = feet.children;
+            for (var i = 0; i < child.length; i++) {
+                child[i].style.boxShadow = '';
+            }
+            for (var i = 17; i < 22; i++) {
+                showheadimg[i].style.visibility = "hidden";
+            }
+            showheadimg[showhead].style.visibility = "visible";
+            event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+            feet_flag = 1;
+            
+        }
+
+    }
+    DefendAdder();
+})
+
+var weapon = document.getElementById("weapon");
+var flag = 0;
+weapon.addEventListener("click", function () {
+    var showhead = parseInt(event.target.id);
+    if (flag == 0) {
+        event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+        showheadimg[showhead].style.visibility = "visible";
+        flag = 1;
+        
+    } else if (flag == 1) {
+        var str1 = "";
+        str1 = event.target.style.boxShadow;
+        if (str1 == "rgba(51, 51, 51, 0.5) 0px 3px 15px 3px") {
+            showheadimg[showhead].style.visibility = "hidden";
+            event.target.style.boxShadow = '';
+            flag = 0;
+            
+        }
+
+        else {
+            var child = weapon.children;
+            for (var i = 0; i < child.length; i++) {
+                var child2 = child[i].children;
+                for (var j = 0; j < child2.length; j++) {
+                    var child3 = child2[j].children;
+                    for (var k = 0; k < child3.length; k++) {
+                        child3[k].style.boxShadow = '';
+                    }
+                }
+            }
+            for (var i = 22; i < 34; i++) {
+                showheadimg[i].style.visibility = "hidden";
+            }
+            showheadimg[showhead].style.visibility = "visible";
+            event.target.style.boxShadow = "0 3px 15px 3px rgba(51, 51, 51, 0.5)";
+            flag = 1;
+            
+        }
+
+    }
+    AttackAdder()
+})
